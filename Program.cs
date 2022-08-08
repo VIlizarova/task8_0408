bbd19f33eb3a4e356663feec26177f78d7d92417
Valeria_Ilizarova <v.ilizarova@gmail.com>
2022-08-08 10:29:41 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine ("Введите число  ");
+
+int number = Convert.ToInt32(Console.ReadLine ());
+
+int even = 2;
+
+string str = "";
+
+while (even <= number)
+{
+str = str + even;
+even = even + 2;
+}
+
+Console.WriteLine(str);
+
+// int even = 2;
+
+// int count = 1;
+
+// int length = 0;
+
+// if (number % 2 == 0)
+// {
+//    length = ((number / 2) - 1); 
+// }
+// else
+// {
+//     length = (((number-1) / 2) - 1);
+// }
+
+// int[] myarray = new int[length];
+
+
+//  while (even <= number)
+// {
+//     myarray.SetValue( even, count );
+//     even = even + 2;
+//     count = count + 1;
+// }
+
+// Array.ForEach(myarray,Console.WriteLine);
